acbacd6d950c40ded59737e32118d3bd8d8f7a9f
focadamore <antoniocalmachiavelli@gmail.com>
2025-06-24 16:22:33 +0300

    add routing

--- hotels.py
+++ hotels.py
@@ -1,28 +1,28 @@
-from fastapi import FastAPI, Query, Body
-import uvicorn
+from fastapi import Body, Query, APIRouter
 
-app = FastAPI()
+router = APIRouter(prefix="/hotels")
+
 hotels = [
     {"id": 1, "title": "Дубай", "name": "Dubai"},
     {"id": 2, "title": "Сочи", "name": "Sochi"},
 ]
 
 
-@app.get("/")
+@router.get("/", summary="Hello page")
 def func():
     """return hello"""
     return {"title": "Hello page!"}
 
 
-@app.delete("/hotels/{hotel_id}")
+@router.delete("/{hotel_id}", summary="Удаление отеля")
 def delete_hotel(id: int):
     global hotels
     hotels = [hotel for hotel in hotels if hotel["id"] != id]
     return {"status": "OK"}
 
 
-@app.put("/hotels/{hotel_id}")
-def change_hotel(id: int,
+@router.put("/{hotel_id}", summary="Изменение всех данных отеля")
+def change_hotel(id: int = Query(description="ID Отеля"),
                  title: str = Body(),
                  name: str = Body()
                  ):
@@ -34,7 +34,7 @@ def change_hotel(id: int,
         return {"status": "404 - Not Found"}
 
 
-@app.patch("/hotels/{hotel_id}")
+@router.patch("/{hotel_id}", summary="Изменение одного или нескольких тэгов отеля")
 def change_hotel_lightly(id: int,
                          title: str | None = Body(None),
                          name: str | None = Body(None)
@@ -47,14 +47,14 @@ def change_hotel_lightly(id: int,
         return {"status": "404 - Not Found"}
 
 
-@app.get("/hotels",
-         summary="Просмотр всех отелей",
-         description="Много много премного текста")
+@router.get("",
+            summary="Просмотр всех отелей",
+            description="Много много премного текста")
 def get_hotels():
     return [hotel for hotel in hotels]
 
 
-@app.post("/hotels")
+@router.post("", summary="Добавление нового отеля")
 def add_hotel(title: str = Body(embed=True)):
     hotels.append(
         {
@@ -62,7 +62,3 @@ def add_hotel(title: str = Body(embed=True)):
             "title": title
         }
     )
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ test.py
@@ -0,0 +1,5 @@
+def get_data(i: int, a: str):
+    pass
+
+
+print(*[get_data(i, "yahoo") for i in range(5)])
