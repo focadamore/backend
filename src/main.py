9bade1fec47297d7e65cc9548a8b1464ab5d4d28
focadamore <antoniocalmachiavelli@gmail.com>
2025-06-25 18:32:43 +0300

    env changes

--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 from src.api.hotels import router as router_hotels
-
+from src.config import settings
 
 app = FastAPI()
 
 app.include_router(router_hotels)
+
+print(f"{settings.DB_URL}")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
