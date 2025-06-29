bb6512393ec5387fb84031c375721602614c68c6
focadamore <antoniocalmachiavelli@gmail.com>
2025-06-29 22:49:39 +0300

    add bookings post correction

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Body
 
 from src.api.dependencies import UserIdDep
 from src.database import async_session_maker
@@ -10,7 +10,11 @@ router = APIRouter(prefix="/auth", tags=["Аутентификация и авт
 
 
 @router.post("/login", summary="Аутентификация")
-async def login_user(data: UsersRegisterAdd, response: Response):
+async def login_user(response: Response, data: UsersRegisterAdd = Body(openapi_examples={
+    "1": {"summary": "Я",
+          "value": {"email": "me@mail.ru",
+                    "password": "123"}}
+})):
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,7 +1,7 @@
 from fastapi import Body, APIRouter
 from sqlalchemy import column
 
-from src.api.dependencies import DBDep
+from src.api.dependencies import DBDep, UserIdDep
 from src.schemas.bookings import BookingsAddRequest, BookingsAdd
 from src.schemas.rooms import RoomsAdd
 
@@ -9,14 +9,9 @@ router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 
 @router.post("", summary="Забронировать номер")
-async def add_booking(db: DBDep, bookings_data: BookingsAddRequest = Body(openapi_examples={
-    "1": {"summary": "тестовая бронь",
-          "value": {"date_from": "2025-07-01",
-                    "date_to": "2025-07-14",
-                    "room_id": "3"
-                    }}
-})):
+async def add_booking(user_id: UserIdDep, db: DBDep, bookings_data: BookingsAddRequest):
     actual_price = await db.rooms.get_one_or_none(id=bookings_data.room_id)
-    _bookings_data = BookingsAdd(**bookings_data.model_dump(), price=actual_price.price, user_id=22)
+    _bookings_data = BookingsAdd(**bookings_data.model_dump(), price=actual_price.price, user_id=user_id)
     booking = await db.bookings.add(_bookings_data)
+    await db.commit()
     return {"status": "OK", "data": booking}
