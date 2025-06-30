41de7c8c6759a5c732d71c811c3b48fb3bcec9b3
focadamore <antoniocalmachiavelli@gmail.com>
2025-06-30 20:21:33 +0300

    add facilities model

---
+++ src/migrations/versions/2025_06_30_2017-ad8f0bb251e9_facilities_migration.py
@@ -0,0 +1,49 @@
+"""facilities migration
+
+Revision ID: ad8f0bb251e9
+Revises: 6ff912c09b83
+Create Date: 2025-06-30 20:17:13.866180
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "ad8f0bb251e9"
+down_revision: Union[str, Sequence[str], None] = "6ff912c09b83"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("rooms_facilities")
+    op.drop_table("facilities")
--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -14,31 +14,9 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotels
 
-    # async def get_all(self, title, location, limit, offset) -> list[Hotels]:
-    #     query = select(HotelsOrm)
-    #     if title:
-    #         query = query.filter(func.lower(HotelsOrm.title).contains(title.lower().strip()))
-    #     if location:
-    #         query = query.filter(func.lower(HotelsOrm.location).contains(location.lower().strip()))
-    #     query = (
-    #         query
-    #         .limit(limit)
-    #         .offset(offset)
-    #     )
-    #     print(query.compile(engine, compile_kwargs={"literal_binds": True}))
-    #     result = await self.session.execute(query)
-    #     return [self.schema.model_validate(model, from_attributes=True) for model in result.scalars().all()]
-
     async def get_filtered_by_time(self, title, location, limit, offset, date_from: date, date_to: date):
         rooms_for_get_filtered = rooms_ids_to_get(date_from=date_from, date_to=date_to)
-        # if title:
-        #     rooms_for_get_filtered = (rooms_for_get_filtered
-        #                               .filter(func.lower(HotelsOrm.title).contains(title.lower().strip())))
-        # if location:
-        #     rooms_for_get_filtered = (rooms_for_get_filtered
-        #                               .filter(func.lower(HotelsOrm.location).contains(location.lower().strip())))
 
-        # get_rooms_in_hotel = get_rooms_in_hotel.filter_by(hotel_id=hotel_id)
         hotels_for_get_filtered = (
             select(RoomsOrm.hotel_id)
             .select_from(RoomsOrm)
@@ -56,4 +34,3 @@ class HotelsRepository(BaseRepository):
                                    .offset(offset))
 
         return await self.get_filtered(HotelsOrm.id.in_(hotels_for_get_filtered))
-
